6093663ffd055ecaf18dfb065715f6c6ba20e5ff
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-11-07 17:59:58 +0530

    Day 2 Challenge

--- Challenges/Day_02.js
+++ Challenges/Day_02.js
@@ -1,3 +1,11 @@
+/*
+## Challenge 2:
+Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word into upper case.
+
+Example string: 'the quick brown fox'
+Expected Output: 'The Quick Brown Fox '
+*/
+
 function uppercaseLetter(str) {
     str = str.split(" ");
     for (var i=0; i<str.length; i++) {
