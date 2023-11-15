0d8a39b2970c0620f66bf6989893b93536603552
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-11-15 21:32:04 +0530

    Day 10 Challenge

---
+++ Challenges/Day_10.js
@@ -0,0 +1,25 @@
+/*
+## Challenge 10
+Write a JavaScript function to apply the Bubble Sort algorithm.  
+Note : According to wikipedia "Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted, comparing each pair of adjacent items and swapping them if they are in the wrong order".  
+Sample array: [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]  
+Expected output: [3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1]
+*/
+
+function bubbleSortDescending(arr) {
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+const sampleArray = [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213];
+const resultArray = bubbleSortDescending(sampleArray);
+console.log(resultArray);
